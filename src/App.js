c0ee3da4365b0623cef5963b8c8012c4b1a91a0a
Sebastian Nykänen <sebastian.nykanen@outlook.com>
2022-02-23 13:46:13 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const handleFetch = (e) => {
+    if (e.key === "Enter") {
+      fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=d791dc08a1799937193437cae5da89fd`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          setWeather(data);
+          setCity("");
+          console.log(data);
+        });
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="search-box">
+        <input
+          onChange={(e) => setCity(e.target.value)}
+          onKeyPress={handleFetch}
+          type="text"
+          value={city}
+          className="search-bar"
+          placeholder="Enter city..."
+        />
+      </div>
+      {typeof weather.sys != "undefined" ? (
+        <>
+          <div className="location">
+            {weather.name}, {weather.sys.country}
+          </div>
+          <div className="temperature">{Math.floor(weather.main.temp)}°C</div>
+        </>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
