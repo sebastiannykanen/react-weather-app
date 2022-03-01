ea14d056e862332563fccb979e21341a1f9dc8cf
Sebastian Nykänen <sebastian.nykanen@outlook.com>
2022-03-01 17:39:52 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,29 +1,44 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import debounce from "lodash.debounce";
 import "./App.css";
 
 function App() {
   const [city, setCity] = useState("");
   const [weather, setWeather] = useState({});
 
-  const handleFetch = (e) => {
-    if (e.key === "Enter") {
-      fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=d791dc08a1799937193437cae5da89fd`
-      )
-        .then((response) => response.json())
-        .then((data) => {
-          setWeather(data);
-          setCity("");
-        });
-    }
+  // const handleFetch = (e) => {
+  //   if (e.key === "Enter") {
+  //     fetch(
+  //       `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=d791dc08a1799937193437cae5da89fd`
+  //     )
+  //       .then((response) => response.json())
+  //       .then((data) => {
+  //         setWeather(data);
+  //         setCity("");
+  //       });
+  //   }
+  // };
+
+  const handleFetch = () => {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=d791dc08a1799937193437cae5da89fd`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setWeather(data);
+      });
   };
+
+  useEffect(() => {
+    debounce(handleFetch, 1000)();
+  }, [city]);
 
   return (
     <div className="App">
       <div className="search-box">
         <input
+          autoFocus
           onChange={(e) => setCity(e.target.value)}
-          onKeyPress={handleFetch}
           type="text"
           value={city}
           className="search-bar"
